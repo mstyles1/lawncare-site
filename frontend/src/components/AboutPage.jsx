b7e7dff8a0f17f5191808c53572943aa90757bd2
matt styles <styles.matt135@gmail.com>
2025-07-02 11:26:14 -0400

    Add AboutPage

---
+++ frontend/src/components/AboutPage.jsx
@@ -0,0 +1,9 @@
+
+export default function AboutPage () {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Steven and Felicity have been taking care of Knoxville's lawns for 2 years. </p>
+        </div>
+    );
+}
