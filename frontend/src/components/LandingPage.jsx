a5a7faffedaed7646a76a941ecbdce94d01bcf05
matt styles <styles.matt135@gmail.com>
2025-07-02 14:23:30 -0400

    Styling and Grass background, fonts

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export default function LandingPage() {
   return (
-    <div>
+    <div className="landing-box">
       <h1>Welcome to LawnCare Co.</h1>
       <p>Family Owned and Operated since </p>
       <Link to="/clientform">Get a Free Estimate</Link>
